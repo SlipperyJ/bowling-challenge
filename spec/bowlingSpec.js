0760823d188f1ca728e3f4679cf13d16b74dce39
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-08-07 23:35:50 +0100

    add complete a frame functionality

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,19 +1,46 @@
 'use strict';
 
-describe("Bowling", function() {
-  var bowling;
+describe("Scorecard", function() {
+  var scorecard;
 
   beforeEach(function() {
-    bowling = new Bowling();
+    scorecard = new Scorecard();
   });
 
-  it('keeps a record of the players score', function() {
-    expect(bowling.score).toEqual(0);
+  it('starts with score at 0', function() {
+    expect(scorecard.scoreTotal).toEqual(0);
+  });
+
+  it ("starts on frame one", function() {
+    expect(scorecard.frameNumber).toEqual(1)
+  });
+
+  it ("starts on roll one", function() {
+    expect(scorecard.rollNumber).toEqual(1)
   });
 
   it ("starts with 10 pins", function() {
-    expect(bowling.pins).toEqual(10)
+    expect(scorecard.pinsNumber).toEqual(10)
   });
 
+  it ("can receive a roll score", function() {
+    scorecard.rollScore(8)
+    expect(scorecard.scoreTotal).toEqual(8)
+  });
 
+  it ("moves to second roll after first roll", function() {
+    scorecard.rollScore(5)
+    expect(scorecard.rollNumber).toEqual(2)
+  });
+
+  it ("deducts pins after first roll", function() {
+    scorecard.rollScore(7)
+    expect(scorecard.pinsNumber).toEqual(3)
+  });
+
+  it ("moves to the next frame after 2 rolls", function(){
+    scorecard.rollScore(7)
+    scorecard.rollScore(2)
+    expect(scorecard.frameNumber).toEqual(2)
+  });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,6 +1,34 @@
 'use strict';
 
-function Bowling() {
-  this.score = 0;
-  this.pins = 10;
+function Scorecard() {
+  this.scoreTotal = 0;
+  this.frameNumber = 1;
+  this.rollNumber = 1;
+  this.pinsNumber = 10;
 }
+
+Scorecard.prototype.getFrame = function () {
+  return this.frameNumber;
+};
+
+Scorecard.prototype.getRoll = function () {
+  return this.rollNumber;
+};
+
+Scorecard.prototype.getPins = function () {
+  return this.pinsNumber
+};
+
+Scorecard.prototype.getScoreTotal = function () {
+  return this.scoreTotal
+};
+
+Scorecard.prototype.rollScore = function (score) {
+  this.scoreTotal += score
+  this.rollNumber += 1
+  this.pinsNumber -= score
+
+  if (this.rollNumber === 2) {
+    this.frameNumber += 1;
+  };
+};
